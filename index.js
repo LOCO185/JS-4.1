e3b8a804fbb91f34894ce7322989b826aa02bfa3
LOCO185 <jloco151@gmail.com>
2021-11-08 21:58:03 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+let numSiblings = prompt('how many siblings do you have?');
+
+if (numSiblings = 1){
+    console.log('1 sibling');
+}
+else if (numSiblings > 1){
+    console.log('More than 1 sibling');
+}
+else {
+    console.log('No siblings');
+}
+// we use triple = operator ,when we need to put string instead of number.
